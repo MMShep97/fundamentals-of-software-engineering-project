d257a55595b9df49184d8b9ac1d473a9069720bc
Marc <MMShep97@gmail.com>
2019-10-15 11:07:52 -0500

    Add api service file, format main

--- client/src/main.js
+++ client/src/main.js
@@ -1,18 +1,31 @@
-import Vue from 'vue'
-import App from './App.vue'
-import VueRouter from 'vue-router'
-import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
-import * as VeeValidate from 'vee-validate'
+import Vue                      from 'vue'
+import App                      from './App.vue'
+import VueRouter                from 'vue-router'
+import BootstrapVue             from 'bootstrap-vue'
+import                               'bootstrap/dist/css/bootstrap.css'
+import                               'bootstrap-vue/dist/bootstrap-vue.css'
+import * as VeeValidate         from 'vee-validate'
+import VueLogger                from 'vuejs-logger'
+
+import { configureFakeBackend } from './_helpers'
+import { store }                from './_store'
+import { router }               from './_helpers/router'
+
+const options = {
+  isEnabled: true,
+  logLevel : 'debug',
+  stringifyArguments : false,
+  showLogLevel : true,
+  showMethodName : false,
+  separator: '|',
+  showConsoleColors: true
+};
 
 Vue.use(VueRouter);
 Vue.use(BootstrapVue);
 Vue.use(VeeValidate);
+Vue.use(VueLogger, options);
 
-import { configureFakeBackend } from './_helpers';
-import { store } from './_store'
-import { router } from './_helpers/router'
 configureFakeBackend();
 
 Vue.config.productionTip = false
