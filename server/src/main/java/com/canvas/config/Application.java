12273140aee4d3c6718b9ba78d57a7fc9fde1b31
Marc <MMShep97@gmail.com>
2019-10-29 17:31:53 -0500

    Re-add cors filter in server

--- server/src/main/java/com/canvas/config/Application.java
+++ server/src/main/java/com/canvas/config/Application.java
@@ -24,5 +24,21 @@ public class Application {
 	@Bean
     public EntityManager entityManager(EntityManagerFactory entityManagerFactory) {
         return entityManagerFactory.createEntityManager();
-	}
+    }
+    
+    // Fix the CORS errors
+    @Bean
+    public FilterRegistrationBean simpleCorsFilter() {  
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();  
+        CorsConfiguration config = new CorsConfiguration();  
+        config.setAllowCredentials(true); 
+        // *** URL below needs to match the Vue client URL and port ***
+        config.setAllowedOrigins(Collections.singletonList("http://localhost:8080")); 
+        config.setAllowedMethods(Collections.singletonList("*"));  
+        config.setAllowedHeaders(Collections.singletonList("*"));  
+        source.registerCorsConfiguration("/**", config);  
+        FilterRegistrationBean bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        bean.setOrder(Ordered.HIGHEST_PRECEDENCE);  
+        return bean;  
+    }
 }
