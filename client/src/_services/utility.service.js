8873354780d387a4f0e1cc00a3007f331200acb2
Marc <MMShep97@gmail.com>
2019-12-09 13:52:15 -0600

    Commented out unused module erroring

--- client/src/_services/utility.service.js
+++ client/src/_services/utility.service.js
@@ -1,30 +1,30 @@
-import {
-    SENDGRID_API_KEY
-} from '.././api-keys'
+// import {
+//     SENDGRID_API_KEY
+// } from '.././api-keys'
 
-const sgMail = require('@sendgrid/mail');
-sgMail.setApiKey(SENDGRID_API_KEY);
+// const sgMail = require('@sendgrid/mail');
+// sgMail.setApiKey(SENDGRID_API_KEY);
 
-function sendEmail(to, name, from, subject, text) {
-    const msg = {
-            to: to,
-            from: from,
-            subject,
-            subject,
-            text: text
-    };
-    // },
-    // function (error, response) {
-    //     //uh oh, there was an error
-    //     if (error) console.log(JSON.stringify(error));
+// function sendEmail(to, name, from, subject, text) {
+//     const msg = {
+//             to: to,
+//             from: from,
+//             subject,
+//             subject,
+//             text: text
+//     };
+//     // },
+//     // function (error, response) {
+//     //     //uh oh, there was an error
+//     //     if (error) console.log(JSON.stringify(error));
 
-    //     //everything's good, lets see what mandrill said
-    //     else console.log(response);
-    // });
+//     //     //everything's good, lets see what mandrill said
+//     //     else console.log(response);
+//     // });
 
-    sgMail.send(msg);
-}
+//     sgMail.send(msg);
+// }
 
-export {
-    sendEmail
-}
+// export {
+//     sendEmail
+// }
